f16fab09e745893aae67341e31b17685c8b2e159
Guosa Edebiri <gedebiri@gmail.com>
2020-04-15 13:53:27 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,6 +2,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |total|
     total.grep(String)
+  end
     total
 end
 
@@ -9,5 +10,6 @@ def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do |total_empty|
     total_empty.grep("")
+  end
     total_empty
 end
